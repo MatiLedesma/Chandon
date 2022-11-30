b96cfd79d46677cd69a74174e209ebc0a3ea3327
franciscor96 <franciscorestivo96@gmail.com>
2022-11-30 13:37:21 -0300

    input radio asociated with the main button container

--- src/components/shape.tsx
+++ src/components/shape.tsx
@@ -4,20 +4,24 @@ import shape1 from "../svg/shape1.svg";
 import shape2 from "../svg/shape2.svg";
 
 export default function shape() {
+
+  
   // This state represent the selected side - 1 for left side / 2 for right side / 0 for unselected
   const [shape, setShape] = useState<number>(0);
+  const [opc1, setOpc1] = useState<boolean>(false)
+  const [opc2, setOpc2] = useState<boolean>(false)
 
   return (
    <div className="main-container">
       <h3>ELEGÍ LA FORMA</h3>
         <div className="inside-container">
-            <button className="input-container">
+            <button className={opc1 ? "input-container__on" : "input-container__off"} onClick={() => setOpc1(!opc1)}>
                     <img className="shape" src={shape1} alt="image" />
-                    <input className="input-radio" type="radio" />
+                    <input checked={opc1} onChange={() => setOpc1(!opc1)} value="2" className="input-radio" type="radio" />
             </button>
-            <button className="input-contariner">
+            <button className={opc2 ? "input-container__on" : "input-container__off"} onClick={() => setOpc2(!opc2)}>
                     <img className="shape" src={shape2} alt="image" />
-                    <input className="input-radio" type="radio" />
+                    <input checked={opc2} onChange={() => setOpc2(!opc2)} value="2" className="input-radio" type="radio" />
             </button>
         </div>
         
