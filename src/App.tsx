8670400cd4a7c7f9d7696960d86d670accf322e6
MatiasLedesma <matias@activity.com.ar>
2022-11-30 20:23:09 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
 import Shape from '../src/components/shape'
 import Msj from '../src/components/msj'
 import Color from '../src/components/color'
 import Start from '../src/components/start'
+import img19 from "./img/16.webp";
+import ImgSlider from './config/ImgSlider';
 
 function App() {
   return (
@@ -13,7 +16,8 @@ function App() {
           <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox"><path d="M0.747,0 H0.714 A0.278,0.153,0,0,0,0.5,0.055 A0.278,0.153,0,0,0,0.286,0 h-0.033 A0.253,0.139,0,0,0,0,0.139 V0.86 A0.254,0.14,0,0,0,0.254,1 h0.024 a0.286,0.157,0,0,0,0.222,-0.058 a0.286,0.157,0,0,0,0.222,0.058 h0.024 A0.254,0.14,0,0,0,1,0.86 V0.139 A0.253,0.139,0,0,0,0.747,0"></path></clipPath>
         </svg>
         <div className="clipped">
-        <Start />
+          {/*<Start />*/}
+          <ImgSlider />
         </div>
       </div>
     </div>
---
+++ src/config/ImgSlider.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react"
+import { images } from "./images"
+
+const ImgSlider = () => {
+    const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+    useEffect(() => {
+
+        (async () => {
+            const clipped = document.querySelector(".clipped");
+            const slider = document.getElementById("slider");
+            if (slider) {
+                slider.style.width = clipped?.clientWidth + "px";
+                slider.style.height = clipped?.clientHeight + "px";
+                slider.style.transition = "all .2s ease-in";
+                slider.style.position = "relative";
+                slider.style.left = "0";
+
+                //adjust image size
+                for (let i = 0; i < slider.children.length; i++) {
+                    const image = document.getElementById(slider.children[i].id);
+                    if (image) {
+                        image.style.width = clipped?.clientWidth + "px";
+                        image.style.height = clipped?.clientHeight + "px";
+                        image.style.objectFit = "cover";
+                    }
+                }
+
+                for (let i = 1; i <= slider.children.length + 1; i++) {
+                    if (!clipped) return;
+                    await wait(2500);
+                    slider.style.left = `-${clipped?.clientWidth * i}px`;
+                    slider.style.opacity = "1";
+                    if (i === slider.children.length) {
+                        i = 1;
+                        slider.style.left = `0px`;
+                        await wait(2500);
+                    }
+                }
+
+                //slider
+
+            }
+        })();
+    }, [])
+
+    return (
+        <div className="d-flex" id="slider">
+            {
+                images.map((value, index) => {
+                    return <img src={value} key={index} className="images" id={"img" + index} alt="chandon image slider" />
+                })
+            }
+        </div>
+    );
+}
+
+export default ImgSlider;
