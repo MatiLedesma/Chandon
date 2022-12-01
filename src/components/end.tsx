4cc131633835d085c9942bb12782e89581ca93a9
franciscor96 <franciscorestivo96@gmail.com>
2022-12-01 13:47:06 -0300

    end.tsx

--- src/components/end.tsx
+++ src/components/end.tsx
@@ -3,12 +3,13 @@ import  img8 from '../img/8.webp';
 
 export default function end() {
   return (
-    <div className='container-start'>
-      <img className="img_carrousel" src={img8} alt="image" />
+    <div className='container-end'>
+      <h1 style={{color: 'white'}}>¡Muchas Gracias!</h1>
       <div className='btn-container'>
       <button className='btn-restart'>
       </button>
       </div>
+      <img className="img_carrousel" src={img8} alt="image" style={{position: 'absolute'}}/>
     </div>
   )
 }
