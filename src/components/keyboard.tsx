99c58fa74257e01b3ac8a3ea82cce7372cb7bbdd
franciscor96 <franciscorestivo96@gmail.com>
2022-12-07 23:33:16 -0300

    max lenght en props del keyboard(funcionalidad de la librería), texto centrado

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -18,12 +18,42 @@ const Keyboard: React.FC<Props> = ({ shape, slideRight, slideLeft, callback }) =
 
   const onChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
     const input: string = event.target.value;
-    setInput(input);
+
+    if (input.length < 50) {
+      setInput(input);
+    }
     keyboard.current.setInput(input);
     console.log(input.length)
     console.log(input)
   };
   let length = input.length
+
+
+
+  // function wrapText(input:string) {   // Esta función se encarga de hacer el corte de palabra
+
+  //   var words = input.split(' ');
+  //   var line = '';
+
+  //   for(var n = 0; n < words.length; n++) {
+  //     var testLine = line + words[n] + ' ';
+  //     var metrics = ctx.measureText(testLine);
+  //     var testWidth = metrics.width;
+  //     if (testWidth > maxWidth && n > 0) {
+  //       ctx.fillText(line, x, y);
+  //       line = words[n] + ' ';
+  //       y += lineHeight;
+  //     }
+  //     else {
+  //       line = testLine;
+  //     }
+  //   }
+  //   ctx.fillText(line, x, y);
+  // }
+
+
+
+
 
   return (
     <div className="main-container">
@@ -32,6 +62,7 @@ const Keyboard: React.FC<Props> = ({ shape, slideRight, slideLeft, callback }) =
         <input maxLength={50} type="text" value={input}
           placeholder={"Toca acá para escribir"}
           onChange={e => onChangeInput(e)} style={{ height: '32vh', marginBottom: '3vh', flexShrink: '1', flexWrap: 'wrap'}} />
+        <textarea value={input}  maxLength={50}   placeholder={"MAXIMO 50 CARACTERES"} className="textInput"><h3>{input}</h3></textarea>
         <KeyboardWrapper keyboardRef={keyboard} onChange={setInput} />
       </div>
       <div className='btn-container'>
